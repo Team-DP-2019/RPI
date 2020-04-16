fdc9b66fca59cf9cee3a0a16abec1f4beb09fc3a
hudanov <scalesymmetry@gmail.com>
2020-04-16 21:54:53 +0300

    Data parsing changed

--- src/main/java/manager/JsonParser.java
+++ src/main/java/manager/JsonParser.java
@@ -1,38 +1,41 @@
-//package manager;
-//
-//import com.google.gson.Gson;
-//import entity.DataEntity;
-//import entity.SensorEntity;
-//
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.JSONParser;
-//
-//public class JsonParser {
-//
-//    public static void parse(String message) {
-//        JSONParser parser = new JSONParser();
-//
-//        try {
-//            Object obj = parser.parse(message);
-//
-//            JSONObject jsonObject = (JSONObject) obj;
+package manager;
+
+import com.google.gson.Gson;
+import entity.DataEntity;
+import entity.SensorEntity;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JsonParser {
+
+    public static void parse(String message) {
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(message);
+
+            JSONObject jsonObject = (JSONObject) obj;
+
 //
 //            String mystring = jsonObject.toString();
 //
 //            Gson gson = new Gson();
 //
 //            SensorEntity sensor = gson.fromJson(mystring, DataEntity.class);
-//
-//            Puller puller = new Puller();
-//
+
+            Puller puller = new Puller();
+
+            puller.insert((Integer) jsonObject.get("id"), (Integer) jsonObject.get("sensorType"), (Integer) jsonObject.get("temperatureValue"));
+
 //            puller.insert(sensor.getId(), sensor.getSensorType(), ((TemperatureSensor) sensor).getTemperatureValue());
-//
+
 //            System.out.print(sensor);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//}
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
